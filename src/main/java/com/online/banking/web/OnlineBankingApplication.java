fb3956a6f80616738fef1e0af55df2246fa153cb
vikas.nandanam <vikas.nandanam@huawei.com>
2018-12-05 09:49:19 -0800

    intial Commit

---
+++ src/main/java/com/online/banking/web/OnlineBankingApplication.java
@@ -0,0 +1,12 @@
+package com.online.banking.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineBankingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineBankingApplication.class, args);
+	}
+}
